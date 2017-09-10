90a983a91785c144651c04935a131a49fc358e67
Soravis Prakkamakul <asunnotthesun@gmail.com>
2017-09-09 23:04:04 -0700

    Motion mapping WIP

--- pages/rooms.js
+++ pages/rooms.js
@@ -11,6 +11,9 @@ import mapMotion from '../lib/motion-mapper'
 const rtm = new RTM('wss://q5241z7b.api.satori.com', 'CD3108D6a79CAE30b8E8C37ebad877A6');
 const channelName = 'jam-session-1'
 
+const MOTION_HISTORY_SIZE = 10;
+const MAX_SAMPLING_RATE = 500;
+
 const ClientConnectedIndicator = styled.div `
   display: inline-block;
   background-color: #00ff00;
@@ -26,6 +29,17 @@ export default class Rooms extends React.Component {
     this.state = {
       clientConnected: false
     };
+
+    this.history = {
+      motion: {
+        acceleration: [],
+        accelerationIncludingGravity: [],
+        rotationRate: []
+      },
+      orientation: []
+    };
+    this.lastMotionTimestamp = new Date();
+
   }
 
   componentDidMount() {
@@ -64,6 +78,15 @@ export default class Rooms extends React.Component {
     this.channel = channel;
     this.rtm = rtm;
 
+    // setInterval(() => {
+    //   this.onMotionOrOrientationChanged(
+    //     { 
+    //       acceleration: { x: Math.random() * 10.0, y: Math.random() * 10.0, z: Math.random() * 10.0 },
+    //       accelerationIncludingGravity: { x: Math.random() * 10.0, y: Math.random() * 10.0, z: Math.random() * 10.0 }
+    //     }, 
+    //     { alpha: Math.random() * 360.0, beta: Math.random() * 360.0 - 180.0, gamma: Math.random() * 360.0 - 180.0 }
+    //   );
+    // }, 10);
   }
 
   onNotePushed(note) {
@@ -90,10 +113,49 @@ export default class Rooms extends React.Component {
     }
   }
 
-  onMotionOrOrientationChanged(motion, orientation) {
-    if(this.midiMachine) {
-      mapMotion(motion, orientation, this.midiMachine.MIDIInstance());
+  updateMotionHistory(motion, orientation) {
+    
+    if(this.history.motion.acceleration.length > MOTION_HISTORY_SIZE) {
+      this.history.motion.acceleration.splice(0, 1);
     }
+    this.history.motion.acceleration.push(motion.acceleration);
+
+    if(this.history.motion.accelerationIncludingGravity.length > MOTION_HISTORY_SIZE) {
+      this.history.motion.accelerationIncludingGravity.splice(0, 1);
+    }
+    this.history.motion.accelerationIncludingGravity.push(motion.accelerationIncludingGravity);
+
+    if(this.history.motion.rotationRate.length > MOTION_HISTORY_SIZE) {
+      this.history.motion.rotationRate.splice(0, 1);
+    }
+    this.history.motion.rotationRate.push(motion.rotationRate);
+
+    if(this.history.orientation.length > MOTION_HISTORY_SIZE) {
+      this.history.orientation.splice(0, 1);
+    }
+    this.history.orientation.push(motion.orientation);
+  }
+
+  onMotionOrOrientationChanged(motion, orientation) {
+    const timeSinceLastMotionTimeStamp = (new Date()) - this.lastMotionTimestamp;
+    if (timeSinceLastMotionTimeStamp > MAX_SAMPLING_RATE) {
+      if(this.midiMachine) {
+        const mid = mapMotion(motion, orientation, this.history.motion, this.history.orientation);
+        if(this.rtm) {
+          this.rtm.publish(channelName, mid , (pdu) => {
+            if (pdu.action === 'rtm/publish/ok') {
+              console.log('Publish confirmed');
+            } else {
+              console.log('Failed to publish. RTM replied with the error ' +
+                  pdu.body.error + ': ' + pdu.body.reason);
+            }
+          });
+        }
+        this.updateMotionHistory(motion, orientation);
+      }
+      this.lastMotionTimestamp = new Date();
+    }
+    
   }
 
   render() {
